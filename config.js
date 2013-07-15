aa868de48ab0e4c49e5c7ecf9dc81df5f911d86c
Brian Mathews <spoonart@gmail.com>
2013-07-14 19:49:30 -0700

    cfg management

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    protocol: 'https',
-    host: "jira.company.com/",
-    port: null,
-    user: 'username',
-    password: 'password'
-};
--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 
 var commander = require('commander'),
     JiraApi = require('jira').JiraApi;
-
-var userConf = require('./config'),
     show = require('./lib/show'),
     create = require('./lib/create'),
     list = require('./lib/list'),
@@ -14,99 +12,103 @@ var userConf = require('./config'),
     transition = require('./lib/transition'),
     assign = require('./lib/assign');
 
-var jira = new JiraApi(userConf.protocol, userConf.host, userConf.port, userConf.user, userConf.password, '2');
+require('./lib/config')(function (config) {
+    var jira = new JiraApi(config.protocol, config.host, config.port, config.user, config.password, '2');
 
-commander
-    .command('projects')
-    .description('list all projects')
-    .action(function (args) {
-        projects(jira, args);
-    });
-commander
-    .command('issuetypes')
-    .description('list all issue types\n')
-    .action(function (args) {
-        issuetypes(jira, args);
-    });
-commander
-    .command('list')
-    .description('list issues')
-    .option('-a, --assignee <user>', "Filter list by assignee")
-    .option('-p, --project <project>', "Filter list by project")
-    .option('-c, --creator <user>', "Filter list by creator")
-    .option('-s, --status <o|c|r|i>', "Filter list by status")
-    .action(function (args) {
-        list(jira, args);
-    });
-commander
-    .command('create')
-    .description('create an issue\n')
-    .option('-i, --interactive', "interactive mode")
-    .option('-a, --assignee <user>', "assignee")
-    .option('-p, --project <project>', "project")
-    .option('-s, --summary <summary>', "title/summary")
-    .option('-d, --desc <desription>', "description")
-    .option('-t, --type <issue type>', "name of issue type")
-    .action(function (args) {
-        create(jira, args);
-    });
-commander
-    .command('show <id>')
-    .description('show an issue by id')
-    .action(function (id) {
-        show(jira, { id: id });
-    });
-
-commander
-    .command('close <id>')
-    .description('close an issue by id')
-    .action(function (args) {
-        transition(jira, { id: id, transitionId: 2 });
-    });
-
-commander
-    .command('resolve <id> ')
-    .description('resolve an issue by id')
-    .action(function (args) {
-        transition(jira, { id: id, transitionId: 3 });
-    });
-
-commander
-    .command('start <id>')
-    .description('set an issue to in progress by id')
-    .action(function (id) {
-        transition(jira, { id: id, transitionId: 4 });
-    });
-commander
-    .command('stop <id>')
-    .description("set an issue to open by id")
-    .action(function (args) {
-        transition(jira, { id: id, transitionId: 301 });
-    });
-commander.command('delete <id>')
-    .description('delete an issue by id\n')
-    .action(function (id) {
-        del(jira, {
-            id: id
+    commander
+        .command('projects')
+        .description('list all projects')
+        .action(function (args) {
+            projects(jira, args);
         });
-    });
-commander
-    .command('comment <id> <text>')
-    .description('comment on an issue')
-    .action(function (id, text) {
-        comment(jira, {
-            id: id,
-            comment: text
+    commander
+        .command('issuetypes')
+        .description('list all issue types\n')
+        .action(function (args) {
+            issuetypes(jira, args);
         });
-    });
-commander.command('assign <id> <user>')
-    .description('assign a user to an issue')
-    .action(function (id, user) {
-        assign(jira, {
-            id: id,
-            user: user
+    commander
+        .command('list')
+        .description('list issues')
+        .option('-a, --assignee <user>', "Filter list by assignee")
+        .option('-p, --project <project>', "Filter list by project")
+        .option('-c, --creator <user>', "Filter list by creator")
+        .option('-s, --status <o|c|r|i>', "Filter list by status")
+        .action(function (args) {
+            list(jira, args);
         });
-    });
+    commander
+        .command('create')
+        .description('create an issue\n')
+        .option('-i, --interactive', "interactive mode")
+        .option('-a, --assignee <user>', "assignee")
+        .option('-p, --project <project>', "project")
+        .option('-s, --summary <summary>', "title/summary")
+        .option('-d, --desc <desription>', "description")
+        .option('-t, --type <issue type>', "name of issue type")
+        .action(function (args) {
+            create(jira, args);
+        });
+    commander
+        .command('show <id>')
+        .description('show an issue by id')
+        .action(function (id) {
+            show(jira, { id: id });
+        });
 
-commander.parse(process.argv);
+    commander
+        .command('close <id>')
+        .description('close an issue by id')
+        .action(function (args) {
+            transition(jira, { id: id, transitionId: 2 });
+        });
+
+    commander
+        .command('resolve <id> ')
+        .description('resolve an issue by id')
+        .action(function (args) {
+            transition(jira, { id: id, transitionId: 3 });
+        });
+
+    commander
+        .command('start <id>')
+        .description('set an issue to in progress by id')
+        .action(function (id) {
+            transition(jira, { id: id, transitionId: 4 });
+        });
+    commander
+        .command('stop <id>')
+        .description("set an issue to open by id")
+        .action(function (args) {
+            transition(jira, { id: id, transitionId: 301 });
+        });
+    commander.command('delete <id>')
+        .description('delete an issue by id\n')
+        .action(function (id) {
+            del(jira, {
+                id: id
+            });
+        });
+    commander
+        .command('comment <id> <text>')
+        .description('comment on an issue')
+        .action(function (id, text) {
+            comment(jira, {
+                id: id,
+                comment: text
+            });
+        });
+    commander.command('assign <id> <user>')
+        .description('assign a user to an issue')
+        .action(function (id, user) {
+            assign(jira, {
+                id: id,
+                user: user
+            });
+        });
+
+    commander.parse(process.argv);
+
+
+});
 
