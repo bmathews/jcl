0cdeabf2861275a3656e221382eb4aba078154fe
Brian Mathews <spoonart@gmail.com>
2013-07-14 22:03:52 -0700

    view comments. improved api. unassign/assign. start/stop auto assign/unassign

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 #!/usr/bin/env node
 
 var commander = require('commander'),
-    JiraApi = require('jira').JiraApi;
-    show = require('./lib/show'),
+    JiraApi = require('jira').JiraApi,
+    basename = require('path').basename;
+
+var show = require('./lib/show'),
     create = require('./lib/create'),
     list = require('./lib/list'),
     projects = require('./lib/projects'),
     issuetypes = require('./lib/issuetypes'),
     comment = require('./lib/comment'),
+    comments = require('./lib/comments'),
     del = require('./lib/del'),
     transition = require('./lib/transition'),
     assign = require('./lib/assign');
 
 require('./lib/config')(function (config) {
     var jira = new JiraApi(config.protocol, config.host, config.port, config.user, config.password, '2');
-
+    commander
+        .command('me')
+        .description('all unresolved issues assigned to you\n')
+        .action(function () {
+            list(jira, {
+                assignee: config.user,
+                unresolved: true
+            });
+        });
     commander
         .command('projects')
         .description('list all projects')
@@ -29,17 +40,19 @@ require('./lib/config')(function (config) {
         });
     commander
         .command('list')
-        .description('list issues')
-        .option('-a, --assignee <user>', "Filter list by assignee")
-        .option('-p, --project <project>', "Filter list by project")
-        .option('-c, --creator <user>', "Filter list by creator")
-        .option('-s, --status <o|c|r|i>', "Filter list by status")
+        .description('list issues       ('+ basename(process.argv[1]) +' list --help)')
+        .option('-o, --open', "show only open issues ")
+        .option('-u, --unresolved', "show only unresolved issues (open, in progress, reopened")
+        .option('-a, --assignee <user>', "filter list by assignee")
+        .option('-p, --project <project>', "filter list by project")
+        .option('-c, --creator <user>', "filter list by creator")
+        .option('-s, --status <o|c|r|i>', "filter list by status")
         .action(function (args) {
             list(jira, args);
         });
     commander
         .command('create')
-        .description('create an issue\n')
+        .description('create an issue   ('+ basename(process.argv[1]) +' create --help)\n')
         .option('-i, --interactive', "interactive mode")
         .option('-a, --assignee <user>', "assignee")
         .option('-p, --project <project>', "project")
@@ -55,32 +68,42 @@ require('./lib/config')(function (config) {
         .action(function (id) {
             show(jira, { id: id });
         });
-
+    commander
+        .command('take <id>')
+        .description('assign an issue to yourself')
+        .action(function (id) {
+            assign(jira, {
+                id: id,
+                user: config.user
+            });
+        });
     commander
         .command('close <id>')
         .description('close an issue by id')
         .action(function (args) {
             transition(jira, { id: id, transitionId: 2 });
         });
-
     commander
         .command('resolve <id> ')
         .description('resolve an issue by id')
         .action(function (args) {
             transition(jira, { id: id, transitionId: 3 });
         });
-
     commander
         .command('start <id>')
-        .description('set an issue to in progress by id')
+        .description('assign to yourself and set an issue to in progress')
         .action(function (id) {
-            transition(jira, { id: id, transitionId: 4 });
+            transition(jira, { id: id, transitionId: 4 }, function () {
+                assign(jira, { id: id, user: config.user });
+            });
         });
     commander
         .command('stop <id>')
-        .description("set an issue to open by id")
-        .action(function (args) {
-            transition(jira, { id: id, transitionId: 301 });
+        .description("unassign and set an issue to open")
+        .action(function (id) {
+            transition(jira, { id: id, transitionId: 301 }, function () {
+                assign(jira, { id: id, user: null});
+            });
         });
     commander.command('delete <id>')
         .description('delete an issue by id\n')
@@ -90,15 +113,24 @@ require('./lib/config')(function (config) {
             });
         });
     commander
+        .command('comments <id>')
+        .description('view comments on an id')
+        .action(function (id) {
+            comments(jira, {
+                id: id
+            });
+        });
+    commander
         .command('comment <id> <text>')
-        .description('comment on an issue')
+        .description('comment on an issue\n')
         .action(function (id, text) {
             comment(jira, {
                 id: id,
                 comment: text
             });
         });
-    commander.command('assign <id> <user>')
+    commander
+        .command('assign <id> <user>')
         .description('assign a user to an issue')
         .action(function (id, user) {
             assign(jira, {
@@ -107,8 +139,40 @@ require('./lib/config')(function (config) {
             });
         });
 
+    commander
+        .command('unassign <id>')
+        .description('unassign a user from an issue\n')
+        .action(function (id, user) {
+            assign(jira, {
+                id: id,
+                user: null
+            });
+        });
+
+    commander
+        .command('* <id>')
+        .description('show an issue by id')
+        .action(function (id) {
+            show(jira, { id: id });
+        });
+
+    commander.on('--help', function(){
+        console.log('  Other:');
+        console.log('');
+        console.log('    $ custom-help --help');
+        console.log('    $ custom-help -h');
+        console.log('');
+    });
+
+
     commander.parse(process.argv);
 
-
+    if (!process.argv[2]) {
+        console.log("\nUnresolved issues assigned to you: ");
+        list(jira, {
+            assignee: config.user,
+            unresolved: true
+        });
+    }
 });
 
